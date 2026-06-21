50074a0604754d7a7ccfb7f1c57bbae55cd95ab0
Tim Zhou <timothyzhoued96@gmail.com>
2026-06-21 09:26:45 -0400

    fix: revert to custom domain configuration and restore CNAME

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://shuxuan-co-ltd.github.io',
-  base: '/shuxuan.github.io'
+  site: 'https://shuxuan.zhoutower.com'
 });
